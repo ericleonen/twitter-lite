8daf24615ef6d90e4982a05fe35d7cb7c2760523
ericleonen <ericleonen@gmail.com>
2022-02-08 17:36:31 -0800

    Create header

---
+++ src/components/header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+export const Header = () => {
+    return (
+        <header className="Header">
+            <h1 className="title">twitter</h1>
+        </header>
+    );
+}
